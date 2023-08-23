b9a9971272c480c578a0a70349684fd0e359f053
ORLANDO ALVES BITTENCOURT NETO <orlando.abneto@senacsp.edu.br>
2023-08-23 08:56:53 -0300

    Projeto Integrador

---
+++ js/script.js
@@ -0,0 +1,26 @@
+/* Carousel */
+
+$(document).ready(function(){
+      
+    // Enable Carousel Indicators
+    $(".item1").click(function(){
+      $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function(){
+      $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function(){
+      $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function(){
+      $("#myCarousel").carousel(3);
+    });
+      
+    // Enable Carousel Controls
+    $(".left").click(function(){
+      $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function(){
+      $("#myCarousel").carousel("next");
+    });
+  });
